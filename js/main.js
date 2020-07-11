0d4c985bd2bf59d208efd9987505555d8ff197b1
kevin_p <kprsd14@gmail.com>
2020-07-10 23:49:18 -0400

    Added toggle button and js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,25 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navBarLinks = document.getElementsByClassName('navbar-links')[0]
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.navbar-links');
+    const navLinks = document.querySelectorAll('.navbar-links li')
 
-toggleButton.addEventListener('click', () => {
-    navBarLinks.classList.toggle('active')
-})
+    
+    burger.addEventListener('click', () => {
+        //Toggle Nav
+        nav.classList.toggle('nav-active');
+
+        //Animate links
+    navLinks.forEach((link, index) => {
+        if(link.style.animation) {
+            link.style.animation = ''
+        } else {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5}s`;
+            }
+        });
+        //Burger Animation
+        burger.classList.toggle('toggle');
+    });
+
+}
+
+navSlide();
